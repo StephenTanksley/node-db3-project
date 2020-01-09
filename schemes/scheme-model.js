0e31165248b0124fd47ad82d9178edb89e2cdbcc
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-01-08 20:38:37 -0600

    Commenting the code from the findSteps section.

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -10,12 +10,15 @@ const findById = (id) => {
         .first()
 }
 
-const findSteps = (id) =>  {
-    return db('steps as t')
-        .join('schemes as s')
-        .on('t.scheme_id', 's.id')
-        .where('t.scheme_id', id)
-        .select('t.id', 's.scheme_name', 't.step_number')
+const findSteps = (id) => {
+    //we're taking in an id as an argument.
+    return db('schemes as t')
+        //we're returning the database table schemes as the left side.
+        .select('s.id', 't.scheme_name', 's.step_number', 's.instructions')
+        //we're choosing what we want to see here.
+        .join('steps as s', 't.id', 's.scheme_id')
+        //we're indicating the table we want to join, the table parameter we'd like to join to, the parameter from the first argument that we'd like to use to join the original table.
+        .where({ scheme_id: id })
 }
 
 const add = async (scheme) => {
